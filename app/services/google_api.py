244db94cbfe16c4620525cadf43e3dd961f1f7da
Андрей Киланов <8941519@mail.ru>
2023-08-31 17:48:09 +0500

    fix spreadsheets_create

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,6 +1,5 @@
 import copy
 from datetime import datetime
-from typing import Optional
 
 from aiogoogle import Aiogoogle
 
@@ -8,43 +7,42 @@ from app.core.config import settings
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
 MAX_CELLS = 26000
-VALUE_ERROR_MESSAGE = 'Превышено допустимое кол-во ячеек!'
+VALUE_ERROR_MESSAGE = (f'Превышено допустимое кол-во ячеек!'
+                       f' Максимум: {MAX_CELLS}')
 TABLE_VALUES = [
     ['Отчет от'],
     ['Топ проектов по скорости закрытия'],
     ['Название проекта', 'Время сбора', 'Описание']
 ]
+SPREADSHEET_PROPERTIES = dict(
+    title='Отчет от ',
+    locale='ru_RU'
+)
+SHEET_PROPERTIES = dict(
+    sheetType='GRID',
+    sheetId=0,
+    title='Лист1',
+    gridProperties=dict(
+        rowCount=0,
+        columnCount=0
+    )
+)
 SPREADSHEET_BODY = dict(
-    properties=dict(
-        title='Отчет от ',
-        locale='ru_RU',
-    ),
-    sheets=[
-        dict(
-            properties=dict(
-                sheetType='GRID',
-                sheetId=0,
-                title='Лист1',
-                gridProperties=dict(
-                    rowCount=0,
-                    columnCount=0
-                )
-            )
-        )
-    ]
+    properties=SPREADSHEET_PROPERTIES,
+    sheets=[dict(properties=SHEET_PROPERTIES)]
 )
 
 
 async def spreadsheets_create(
         projects: list,
         wrapper_services: Aiogoogle
-) -> list[Optional[int]]:
-    service = await wrapper_services.discover('sheets', 'v4')
-    spreadsheet_body = copy.deepcopy(SPREADSHEET_BODY)
+) -> list[str | int]:
     count_rows = len(projects) + len(TABLE_VALUES)
     count_columns = max(map(len, TABLE_VALUES))
     if (count_rows * count_columns) > MAX_CELLS:
         raise ValueError(VALUE_ERROR_MESSAGE)
+    service = await wrapper_services.discover('sheets', 'v4')
+    spreadsheet_body = copy.deepcopy(SPREADSHEET_BODY)
     spreadsheet_body['properties']['title'] += datetime.now().strftime(FORMAT)
     spreadsheet_body['sheets'][0]['properties']['gridProperties'].update(
         dict(rowCount=count_rows, columnCount=count_columns)
@@ -87,19 +85,19 @@ async def spreadsheets_update_value(
         wrapper_services: Aiogoogle
 ) -> None:
     service = await wrapper_services.discover('sheets', 'v4')
-    table_value = [
+    table_values = [
         *copy.deepcopy(TABLE_VALUES),
         *[list(map(
             str,
             [res.name, (res.close_date - res.create_date), res.description])
         )for res in projects]
     ]
-    table_value[0].append(datetime.now().strftime(FORMAT))
+    table_values[0].append(datetime.now().strftime(FORMAT))
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
             spreadsheetId=spreadsheet_id,
             range=f'R1C1:R{count_rows}C{count_columns}',
             valueInputOption='USER_ENTERED',
-            json=dict(majorDimension='ROWS', values=table_value)
+            json=dict(majorDimension='ROWS', values=table_values)
         )
     )
